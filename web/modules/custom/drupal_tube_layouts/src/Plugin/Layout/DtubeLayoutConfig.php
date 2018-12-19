4ce3af1a97699b5a2db9232d93c9b9998bb36b2c
Ashraf Abed <Ashraf.Abed@debugacademy.com>
2018-12-19 01:15:19 +0000

    Made padding & bg colors configurable for new layout sections

--- web/modules/custom/drupal_tube_layouts/src/Plugin/Layout/DtubeLayoutConfig.php
+++ web/modules/custom/drupal_tube_layouts/src/Plugin/Layout/DtubeLayoutConfig.php
@@ -10,17 +10,41 @@ use Drupal\Core\Plugin\PluginFormInterface;
  * Class DtubeLayoutConfig
  */
 class DtubeLayoutConfig extends LayoutDefault implements PluginFormInterface {
+  public function build(array $regions) {
+    $build = parent::build($regions);
+    $build['#attributes']['class'][] = $this->configuration['bgcolor'];
+    if (!empty($this->configuration['spacing'])) {
+      foreach ($this->configuration['spacing'] as $padding_prop) {
+        $build['#attributes']['class'][] = $padding_prop;
+      }
+    }
+    return $build;
+  }
+  
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
     $bg_values = [
-      'bg-primary' => t('Primary (White)'),
-      'bg-secondary' => t('Secondary (Biege)'),
-      'bg-featured' => t('Featured (Gray)'),
+      'lb-bg-primary' => t('Primary (White)'),
+      'lb-bg-secondary' => t('Secondary (Biege)'),
+      'lb-bg-featured' => t('Featured (Gray)'),
     ];
     $form['bgcolor'] = [
       '#type' => 'select',
       '#title' => 'Section Background color',
       '#options' => $bg_values,
       '#default_value' => $this->configuration['bgcolor'],
+    ];
+    
+    $padding_options = [
+      'space-top' => t('Space above content'),
+      'space-bottom' => t('Space below content'),
+      'space-between' => t('Space between content'),
+      'space-beside' => t('Space beside section'),
+    ];
+    $form['spacing'] = [
+      '#type' => 'checkboxes',
+      '#title' => 'Section Padding',
+      '#options' => $padding_options,
+      '#default_value' => $this->configuration['spacing'],
     ];
     
     return $form;
@@ -31,11 +55,16 @@ class DtubeLayoutConfig extends LayoutDefault implements PluginFormInterface {
   }
 
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $this->configuration['bgcolor'] = $form_state->getValue('bgcolor');
     
+    $spacing_values = $form_state->getValue('spacing');
+    foreach ($spacing_values as $index => $value) {
+      if (empty($spacing_values[$index])) {
+        unset($spacing_values[$index]);
+      }
+    }
+    $this->configuration['spacing'] = $spacing_values;
   }
-  
-
-
 
   
 }
